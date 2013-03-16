6cd1b24a34a4a8e9e4341767516427e59b41c2ee
Wojciech Migda <wojtek.golf@interia.pl>
2013-03-16 19:15:04 +0100

    pcap_feeder beautifying and cxxtest find

---
+++ connectivity/pcap-feed/src/i_pcap_lib.h
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Wojciech Migda
+ * All Rights Reserved.
+ * Distributed under the terms of the GNU LGPL v3
+ *******************************************************************************
+ *
+ * Filename: i_pcap_lib.h
+ *
+ * Description:
+ *      PCAP lib interface
+ *
+ * Authors:
+ *          Wojciech Migda (wm)
+ *
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 16-03-2013   wm              Initial version
+ *
+ ******************************************************************************/
+
+
+#ifndef I_PCAP_LIB_H_
+#define I_PCAP_LIB_H_
+
+#include <stdio.h>
+#include <pcap/pcap.h>
+
+/**
+ * pcap library interface type
+ */
+typedef struct
+{
+    pcap_t *    (*fopen_offline)(FILE *, char *);
+    int         (*next_ex)(pcap_t *, struct pcap_pkthdr **, const u_char **);
+} i_pcap_lib_t;
+
+extern i_pcap_lib_t    i_pcap_lib;
+
+#endif /* I_PCAP_LIB_H_ */
