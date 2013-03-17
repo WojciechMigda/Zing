004d09ed946f5fd30dbd41d9bbc26f9669d9a369
Wojciech Migda <wojtek.golf@interia.pl>
2013-03-17 19:53:25 +0100

    inet proto unpack unit tests

--- connectivity/inet-proto/src/eth_unpack.c
+++ connectivity/inet-proto/src/eth_unpack.c
@@ -1,11 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Wojciech Migda
+ * All Rights Reserved.
+ * Distributed under the terms of the GNU LGPL v3
+ *******************************************************************************
+ *
+ * Filename: eth_unpack.c
+ *
+ * Description:
+ *      eth_unitdata_unpack interface implementation
+ *
+ * Authors:
+ *          Wojciech Migda (wm)
+ *
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 2013-03-16   wm              Initial version
+ *
+ ******************************************************************************/
+
 #include <stdint.h>
 #include <stddef.h>
 #include <net/ethernet.h>
 #include <arpa/inet.h>
 
-#include "eth_unpack.h"
 #include <netinet/ip.h>
 
+#include "eth_unpack.h"
+#include "inet_unpack_status.h"
+
+/*******************************************************************************
+ * @brief unpack ethernet header from the packet
+ *******************************************************************************
+ * Unpack ethernet header from the supplied packet. Error is reported when the
+ * input pointer is NULL or the amount of data is not sufficient. User has to
+ * make sure that the space where the header will be unpacked is large enough
+ * to hold entire header data.
+ * Areas where @c out_eth_header_p and @c out_offset_p point to will be modified
+ * only when they are not NULL and no error occurs.
+ * Unpacked header has host's endianness.
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 17-03-2013   wm              Initial version
+ *
+ *******************************************************************************
+ * @param in_data_p pointer to the input packet data
+ * @param in_size amount of octets in the input packet
+ * @packet out_eth_header_p pointer to the space where the header will be
+ *         unpacked. May be NULL.
+ * @param out_offset_p pointer where number of unpacked octets will be stored.
+ *        May be NULL.
+ * @return status of unpacking - @c inet_proto_unpack_status_values
+ ******************************************************************************/
 int eth_unitdata_unpack(
         const uint8_t * in_data_p,
         const size_t in_size,
@@ -16,17 +67,18 @@ int eth_unitdata_unpack(
 
     if (NULL == in_data_p)
     {
-        return -1;
+        return INET_PROTO_UNPACK_NULL_PACKET_PTR;
     }
 
     if (in_size < sizeof (eth_header_t))
     {
-        return -2;
+        return INET_PROTO_UNPACK_PACKET_TOO_SHORT;
     }
 
     if (out_eth_header_p != NULL)
     {
         *out_eth_header_p = *((eth_header_t *)in_data_p);
+
         out_eth_header_p->ether_type = ntohs(out_eth_header_p->ether_type);
     }
     if (out_offset_p != NULL)
@@ -34,16 +86,5 @@ int eth_unitdata_unpack(
         *out_offset_p = sizeof (eth_header_t);
     }
 
-    return 0;
-}
-
-void eth_unitdata_ind_handler(const uint8_t * in_data_p, const size_t in_size, void *user_data_p)
-{
-    size_t          nbytes_read;
-    eth_header_t    eth_header;
-
-    if (eth_unitdata_unpack(in_data_p, in_size, &eth_header, &nbytes_read) != 0)
-    {
-        return;
-    }
+    return INET_PROTO_UNPACK_SUCCESS;
 }
--- connectivity/inet-proto/src/ip_unpack.c
+++ connectivity/inet-proto/src/ip_unpack.c
@@ -1,16 +1,61 @@
-/*
- * ip_unpack.c
+/*******************************************************************************
+ * Copyright (c) 2013, Wojciech Migda
+ * All Rights Reserved.
+ * Distributed under the terms of the GNU LGPL v3
+ *******************************************************************************
  *
- *  Created on: 15-03-2013
- *      Author: wojtek
- */
+ * Filename: ip_unpack.c
+ *
+ * Description:
+ *      ip_unitdata_unpack interface implementation
+ *
+ * Authors:
+ *          Wojciech Migda (wm)
+ *
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 2013-03-16   wm              Initial version
+ *
+ ******************************************************************************/
 
 #include <stdint.h>
 #include <stddef.h>
 #include <arpa/inet.h>
 
 #include "ip_unpack.h"
+#include "inet_unpack_status.h"
 
+/*******************************************************************************
+ * @brief unpack IP header from the packet
+ *******************************************************************************
+ * Unpack IP header from the supplied packet. Error is reported when the
+ * input pointer is NULL or the amount of data is not sufficient. User has to
+ * make sure that the space where the header will be unpacked is large enough
+ * to hold entire header data.
+ * Areas where @c out_ip_header_p and @c out_offset_p point to will be modified
+ * only when they are not NULL and no error occurs.
+ * Unpacked header has host's endianness.
+ *
+ * NOTE: only mandatory (non-optional) part of the header is unpacked.
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 17-03-2013   wm              Initial version
+ *
+ *******************************************************************************
+ * @param in_data_p pointer to the input packet data
+ * @param in_size amount of octets in the input packet
+ * @packet out_ip_header_p pointer to the space where the header will be
+ *         unpacked. May be NULL.
+ * @param out_offset_p pointer where number of unpacked octets will be stored.
+ *        May be NULL.
+ * @return status of unpacking - @c inet_proto_unpack_status_values
+ ******************************************************************************/
 int ip_unitdata_unpack(
     const uint8_t * in_data_p,
     const size_t in_size,
@@ -21,17 +66,18 @@ int ip_unitdata_unpack(
 
     if (NULL == in_data_p)
     {
-        return -1;
+        return INET_PROTO_UNPACK_NULL_PACKET_PTR;
     }
 
     if (in_size < sizeof (ip_header_t))
     {
-        return -2;
+        return INET_PROTO_UNPACK_PACKET_TOO_SHORT;
     }
 
     if (out_ip_header_p != NULL)
     {
         *out_ip_header_p = *((ip_header_t *)in_data_p);
+
         out_ip_header_p->tot_len = ntohs(out_ip_header_p->tot_len);
         out_ip_header_p->id = ntohs(out_ip_header_p->id);
         out_ip_header_p->frag_off = ntohs(out_ip_header_p->frag_off);
@@ -44,5 +90,5 @@ int ip_unitdata_unpack(
         *out_offset_p = sizeof (ip_header_t);
     }
 
-    return 0;
+    return INET_PROTO_UNPACK_SUCCESS;
 }
