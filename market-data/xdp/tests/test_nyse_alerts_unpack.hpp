d08f04954c6b1e8e455aaf0cc85a9ad13b30a0c6
Wojciech Migda <wojtek.golf@interia.pl>
2013-03-20 22:53:21 +0100

    TDD: unpack nyse alerts - ut

--- market-data/xdp/tests/test_nyse_alerts_unpack.hpp
+++ market-data/xdp/tests/test_nyse_alerts_unpack.hpp
@@ -28,14 +28,51 @@
 #include <stddef.h>
 #include <ctime>
 #include <endian.h>
+#include <algorithm>
+#include <string.h>
 
 #include "nyse_alerts_unpack.h"
+#include "serialize.h"
 
 class NyseAlertsUnpack : public CxxTest::TestSuite
 {
 private:
 
 testing::internal::Random * m_random;
+
+void compose_random_pdp_header(nyse_alerts_pdp_header_t * const out_p)
+{
+#define ASSIGN_RAND(x) do{x = m_random->Generate(std::min((size_t)std::numeric_limits<typeof(x)>::max(), (size_t)m_random->kMaxRange));}while(0)
+    ASSIGN_RAND(out_p->msg_size);
+    ASSIGN_RAND(out_p->msg_type);
+    ASSIGN_RAND(out_p->msg_seq_num);
+    ASSIGN_RAND(out_p->send_time);
+    ASSIGN_RAND(out_p->product_id);
+    ASSIGN_RAND(out_p->retrans_flag);
+    ASSIGN_RAND(out_p->num_body_entries);
+    ASSIGN_RAND(out_p->filler);
+#undef ASSIGN_RAND
+}
+
+uint8_t * format_pdp_header(nyse_alerts_pdp_header_t const * const i_hdr_p, uint8_t * o_buffer, const size_t o_buf_size)
+{
+    assert(o_buf_size >= NYSE_ALERTS_PDP_HEADER_SIZE);
+
+    size_t      random_offset =
+        m_random->Generate(1 + o_buf_size - NYSE_ALERTS_PDP_HEADER_SIZE);
+
+    __serialize_be16(i_hdr_p->msg_size,     &o_buffer[random_offset + 0]);
+    __serialize_be16(i_hdr_p->msg_type,     &o_buffer[random_offset + 2]);
+    __serialize_be32(i_hdr_p->msg_seq_num,  &o_buffer[random_offset + 4]);
+    __serialize_be32(i_hdr_p->send_time,    &o_buffer[random_offset + 8]);
+
+    o_buffer[random_offset + 12] = i_hdr_p->product_id;
+    o_buffer[random_offset + 13] = i_hdr_p->retrans_flag;
+    o_buffer[random_offset + 14] = i_hdr_p->num_body_entries;
+    o_buffer[random_offset + 15] = i_hdr_p->filler;
+
+    return &o_buffer[random_offset];
+}
 
 public:
 
@@ -50,7 +87,7 @@ NyseAlertsUnpack()
 
 void test_00100_fails_when_input_packet_ptr_is_null(void)
 {
-    size_t                      in_size;
+    size_t                      in_size = m_random->Generate(m_random->kMaxRange);
 
     nyse_alerts_pdp_header_t    out_data;
     size_t                      out_size;
@@ -77,9 +114,16 @@ void test_00101_fails_when_input_packet_is_too_short(void)
 
 void test_00200_proper_packet_is_unpacked(void)
 {
-    nyse_alerts_pdp_header_t    in_packet;
+    nyse_alerts_pdp_header_t    in_data;
+
+    compose_random_pdp_header(&in_data);
+
+    uint8_t                     in_packet[2 * NYSE_ALERTS_PDP_HEADER_SIZE];
     uint8_t *                   in_packet_p;
-    size_t                      in_size;
+
+    in_packet_p = format_pdp_header(&in_data, in_packet, sizeof (in_packet));
+
+    size_t                      in_size = NYSE_ALERTS_PDP_HEADER_SIZE + m_random->Generate(NYSE_ALERTS_PDP_HEADER_SIZE);
 
     nyse_alerts_pdp_header_t    out_data;
     size_t                      out_size;
@@ -89,15 +133,15 @@ void test_00200_proper_packet_is_unpacked(void)
 
     TS_ASSERT_EQUALS(XDP_UNPACK_SUCCESS, result);
 
-    TS_ASSERT_EQUALS(out_data.msg_size,         be16toh(in_packet.msg_size));
-    TS_ASSERT_EQUALS(out_data.msg_type,         be16toh(in_packet.msg_type));
-    TS_ASSERT_EQUALS(out_data.msg_seq_num,      be32toh(in_packet.msg_seq_num));
-    TS_ASSERT_EQUALS(out_data.send_time,        be32toh(in_packet.send_time));
-    TS_ASSERT_EQUALS(out_data.product_id,       in_packet.product_id);
-    TS_ASSERT_EQUALS(out_data.retrans_flag,     in_packet.retrans_flag);
-    TS_ASSERT_EQUALS(out_data.num_body_entries, in_packet.num_body_entries);
+    TS_ASSERT_EQUALS(out_data.msg_size,         be16toh(in_data.msg_size));
+    TS_ASSERT_EQUALS(out_data.msg_type,         be16toh(in_data.msg_type));
+    TS_ASSERT_EQUALS(out_data.msg_seq_num,      be32toh(in_data.msg_seq_num));
+    TS_ASSERT_EQUALS(out_data.send_time,        be32toh(in_data.send_time));
+    TS_ASSERT_EQUALS(out_data.product_id,       in_data.product_id);
+    TS_ASSERT_EQUALS(out_data.retrans_flag,     in_data.retrans_flag);
+    TS_ASSERT_EQUALS(out_data.num_body_entries, in_data.num_body_entries);
 
-    TS_ASSERT_EQUALS(out_size, sizeof (nyse_alerts_pdp_header_t));
+    TS_ASSERT_EQUALS(out_size, (size_t)NYSE_ALERTS_PDP_HEADER_SIZE);
 }
 
 };
