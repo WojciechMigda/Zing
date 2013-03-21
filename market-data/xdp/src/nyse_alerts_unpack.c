e94da9c2641f99c180144601a5c25dea5c85283c
Wojciech Migda <wojtek.golf@interia.pl>
2013-03-21 20:36:16 +0100

    indication implementation - green

--- market-data/xdp/src/nyse_alerts_unpack.c
+++ market-data/xdp/src/nyse_alerts_unpack.c
@@ -261,5 +261,28 @@ int nyse_alerts_unpack_indication_msg(
     nyse_alerts_indication_msg_t * const RESTRICT out_body_p,
     size_t * const RESTRICT out_offset_p)
 {
+    if (NULL == in_data_p)
+    {
+        return XDP_UNPACK_NULL_INPUT_PACKET_PTR;
+    }
+
+    if (in_size < NYSE_ALERTS_INDICATION_MSG_SIZE)
+    {
+        return XDP_UNPACK_INPUT_PACKET_TOO_SHORT;
+    }
+
+    if (out_offset_p != NULL)
+    {
+        *out_offset_p = NYSE_ALERTS_INDICATION_MSG_SIZE;
+    }
+    if (out_body_p != NULL)
+    {
+        *out_body_p = *((nyse_alerts_indication_msg_t *)in_data_p);
+
+        out_body_p->source_time =           be32toh(out_body_p->source_time);
+        out_body_p->bid_price =             be32toh(out_body_p->bid_price);
+        out_body_p->ask_price =             be32toh(out_body_p->ask_price);
+    }
+
     return XDP_UNPACK_SUCCESS;
 }
