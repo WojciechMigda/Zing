f90e2bf81f826d47b8e8f7934bff698f05919bba
Wojciech Migda <wojtek.golf@interia.pl>
2013-03-21 23:43:32 +0100

    bbo quote implementation - green

--- market-data/xdp/src/nyse_bbo_unpack.c
+++ market-data/xdp/src/nyse_bbo_unpack.c
@@ -107,5 +107,30 @@ int nyse_bbo_unpack_quote_msg(
     nyse_bbo_quote_msg_t * const RESTRICT out_body_p,
     size_t * const RESTRICT out_offset_p)
 {
+    if (NULL == in_data_p)
+    {
+        return XDP_UNPACK_NULL_INPUT_PACKET_PTR;
+    }
+
+    if (in_size < NYSE_BBO_QUOTE_MSG_SIZE)
+    {
+        return XDP_UNPACK_INPUT_PACKET_TOO_SHORT;
+    }
+
+    if (out_offset_p != NULL)
+    {
+        *out_offset_p = NYSE_BBO_QUOTE_MSG_SIZE;
+    }
+    if (out_body_p != NULL)
+    {
+        *out_body_p = *((nyse_bbo_quote_msg_t *)in_data_p);
+
+        out_body_p->source_time =           be32toh(out_body_p->source_time);
+        out_body_p->ask_price_nominator =   be32toh(out_body_p->ask_price_nominator);
+        out_body_p->ask_size =              be32toh(out_body_p->ask_size);
+        out_body_p->bid_price_numerator =   be32toh(out_body_p->bid_price_numerator);
+        out_body_p->bid_size =              be32toh(out_body_p->bid_size);
+    }
+
     return XDP_UNPACK_SUCCESS;
 }
