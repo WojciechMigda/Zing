29d65bbe728f0200721f64eabe67960ae84d96ca
Wojciech Migda <wojtek.golf@interia.pl>
2013-03-21 20:24:54 +0100

    delay halts implementation - green

--- market-data/xdp/src/nyse_alerts_unpack.c
+++ market-data/xdp/src/nyse_alerts_unpack.c
@@ -212,5 +212,26 @@ int nyse_alerts_unpack_delay_halt_msg(
     nyse_alerts_delay_halts_msg_t * const RESTRICT out_body_p,
     size_t * const RESTRICT out_offset_p)
 {
+    if (NULL == in_data_p)
+    {
+        return XDP_UNPACK_NULL_INPUT_PACKET_PTR;
+    }
+
+    if (in_size < NYSE_ALERTS_DELAY_HALTS_MSG_SIZE)
+    {
+        return XDP_UNPACK_INPUT_PACKET_TOO_SHORT;
+    }
+
+    if (out_offset_p != NULL)
+    {
+        *out_offset_p = NYSE_ALERTS_DELAY_HALTS_MSG_SIZE;
+    }
+    if (out_body_p != NULL)
+    {
+        *out_body_p = *((nyse_alerts_delay_halts_msg_t *)in_data_p);
+
+        out_body_p->source_time =           be32toh(out_body_p->source_time);
+    }
+
     return XDP_UNPACK_SUCCESS;
 }
