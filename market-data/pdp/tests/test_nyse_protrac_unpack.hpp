1e96e5112e99f3dcdd41de096a8ada1c219ec412
Wojciech Migda <wojtek.golf@interia.pl>
2013-03-25 22:02:14 +0100

    protrac testcases

---
+++ market-data/pdp/tests/test_nyse_protrac_unpack.hpp
@@ -0,0 +1,383 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Wojciech Migda
+ * All rights reserved
+ * Distributed under the terms of the GNU LGPL v3
+ *******************************************************************************
+ *
+ * Filename: test_nyse_protrac_unpack.hpp
+ *
+ * Description:
+ *      Testcases for the NYSE Order Imbalances unpacking routines
+ *
+ * Authors:
+ *          Wojciech Migda (wm)
+ *
+ *******************************************************************************
+ * History:
+ * --------
+ * Date         Who  Ticket     Description
+ * ----------   ---  ---------  ------------------------------------------------
+ * 2013-03-25   wm              Initial version
+ *
+ ******************************************************************************/
+
+
+#include <cxxtest/TestSuite.h>
+#include <gmock/gmock.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <cassert>
+
+#include "anon_var.hpp"
+#include "serializer.hpp"
+#include "unpack_status.h"
+#include "gen_test.hpp"
+
+#include "nyse_protrac_unpack.h"
+#include "nyse_protrac_pdp_header.h"
+#include "nyse_protrac_execution_report.h"
+#include "nyse_protrac_execution_report_cancellation.h"
+#include "nyse_protrac_summary.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+void compose_random_pdp_header(nyse_protrac_pdp_header_t * const out_p)
+{
+    ANON_VAR(out_p->msg_size);
+    ANON_VAR(out_p->msg_type);
+    ANON_VAR(out_p->msg_seq_num);
+    ANON_VAR(out_p->send_time);
+    ANON_VAR(out_p->product_id);
+    ANON_VAR(out_p->retrans_flag);
+    ANON_VAR(out_p->num_body_entries);
+    ANON_ARRAY(out_p->FILLER);
+}
+
+uint8_t * format_pdp_header(nyse_protrac_pdp_header_t const * const i_hdr_p, uint8_t * o_buffer, const size_t o_buf_size)
+{
+    assert(o_buf_size >= NYSE_PROTRAC_PDP_HEADER_SIZE);
+
+    size_t      random_offset = random() % (1 + o_buf_size - NYSE_PROTRAC_PDP_HEADER_SIZE);
+
+    std::vector<uint8_t>    work_vec;
+
+    serialize(i_hdr_p->msg_size, work_vec);
+    serialize(i_hdr_p->msg_type, work_vec);
+    serialize(i_hdr_p->msg_seq_num, work_vec);
+    serialize(i_hdr_p->send_time, work_vec);
+    serialize(i_hdr_p->product_id, work_vec);
+    serialize(i_hdr_p->retrans_flag, work_vec);
+    serialize(i_hdr_p->num_body_entries, work_vec);
+    work_vec.insert(work_vec.end(), i_hdr_p->FILLER, i_hdr_p->FILLER + sizeof (i_hdr_p->FILLER));
+
+    std::copy(work_vec.begin(), work_vec.end(), &o_buffer[random_offset]);
+
+    return &o_buffer[random_offset];
+}
+
+void assert_pdp_headers_are_equal(const nyse_protrac_pdp_header_t & lhs, const nyse_protrac_pdp_header_t & rhs)
+{
+    TS_ASSERT_EQUALS(lhs.msg_size,          rhs.msg_size);
+    TS_ASSERT_EQUALS(lhs.msg_type,          rhs.msg_type);
+    TS_ASSERT_EQUALS(lhs.msg_seq_num,       rhs.msg_seq_num);
+    TS_ASSERT_EQUALS(lhs.send_time,         rhs.send_time);
+    TS_ASSERT_EQUALS(lhs.product_id,        rhs.product_id);
+    TS_ASSERT_EQUALS(lhs.retrans_flag,      rhs.retrans_flag);
+    TS_ASSERT_EQUALS(lhs.num_body_entries,  rhs.num_body_entries);
+    TS_ASSERT_SAME_DATA(lhs.FILLER, rhs.FILLER, sizeof (lhs.FILLER));
+}
+
+class NyseProtracUnpackPdpHeader : public CxxTest::TestSuite
+{
+private:
+
+testing::internal::Random * m_random;
+
+public:
+NyseProtracUnpackPdpHeader()
+{
+    m_random = new testing::internal::Random(time(0));
+    srandom(time(0));
+}
+~NyseProtracUnpackPdpHeader()
+{
+    delete m_random;
+}
+
+void test_00100_fails_when_input_packet_ptr_is_null(void)
+{
+    gen_test_input_packet_ptr_is_null<nyse_protrac_pdp_header_t>
+        (nyse_protrac_unpack_pdp_header, m_random);
+}
+
+void test_00101_fails_when_input_packet_is_too_short(void)
+{
+    gen_test_input_packet_is_too_short<nyse_protrac_pdp_header_t>
+        (nyse_protrac_unpack_pdp_header, NYSE_PROTRAC_PDP_HEADER_SIZE, m_random);
+}
+
+void test_00200_proper_packet_is_unpacked(void)
+{
+    gen_test_input_packet_is_unpacked<nyse_protrac_pdp_header_t>
+        (
+            compose_random_pdp_header,
+            format_pdp_header,
+            nyse_protrac_unpack_pdp_header,
+            assert_pdp_headers_are_equal,
+            NYSE_PROTRAC_PDP_HEADER_SIZE,
+            m_random);
+}
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+void compose_random_msg(nyse_protrac_execution_report_msg_t * const out_p)
+{
+    ANON_VAR(out_p->exec_time);
+    ANON_ARRAY(out_p->symbol);
+    ANON_VAR(out_p->volume);
+    ANON_VAR(out_p->link_id);
+    ANON_VAR(out_p->execution_type);
+}
+
+uint8_t * format_message(
+    nyse_protrac_execution_report_msg_t const * const i_msg_p,
+    uint8_t * o_buffer,
+    const size_t o_buf_size)
+{
+    assert(o_buf_size >= NYSE_PROTRAC_EXECUTION_REPORT_MSG_SIZE);
+
+    size_t      random_offset = random() % (1 + o_buf_size - NYSE_PROTRAC_EXECUTION_REPORT_MSG_SIZE);
+
+    std::vector<uint8_t>    work_vec;
+
+    serialize(i_msg_p->exec_time, work_vec);
+    work_vec.insert(work_vec.end(), i_msg_p->symbol, i_msg_p->symbol + sizeof (i_msg_p->symbol));
+    serialize(i_msg_p->volume, work_vec);
+    serialize(i_msg_p->link_id, work_vec);
+    serialize(i_msg_p->execution_type, work_vec);
+
+    std::copy(work_vec.begin(), work_vec.end(), &o_buffer[random_offset]);
+
+    return &o_buffer[random_offset];
+}
+
+void assert_messages_are_equal(const nyse_protrac_execution_report_msg_t & lhs, const nyse_protrac_execution_report_msg_t & rhs)
+{
+    TS_ASSERT_EQUALS(lhs.exec_time,             rhs.exec_time);
+    TS_ASSERT_SAME_DATA(lhs.symbol, rhs.symbol, sizeof (lhs.symbol));
+    TS_ASSERT_EQUALS(lhs.volume,                rhs.volume);
+    TS_ASSERT_EQUALS(lhs.link_id,               rhs.link_id);
+    TS_ASSERT_EQUALS(lhs.execution_type,        rhs.execution_type);
+}
+
+class NyseProtracUnpackExecutionReportMsg : public CxxTest::TestSuite
+{
+private:
+
+testing::internal::Random * m_random;
+
+public:
+
+NyseProtracUnpackExecutionReportMsg()
+{
+    m_random = new testing::internal::Random(time(0));
+    srandom(time(0));
+}
+~NyseProtracUnpackExecutionReportMsg()
+{
+    delete m_random;
+}
+
+void test_00100_fails_when_input_packet_ptr_is_null(void)
+{
+    gen_test_input_packet_ptr_is_null<nyse_protrac_execution_report_msg_t>
+        (nyse_protrac_unpack_execution_report_msg, m_random);
+}
+
+void test_00101_fails_when_input_packet_is_too_short(void)
+{
+    gen_test_input_packet_is_too_short<nyse_protrac_execution_report_msg_t>
+        (nyse_protrac_unpack_execution_report_msg, NYSE_PROTRAC_EXECUTION_REPORT_MSG_SIZE, m_random);
+}
+
+void test_00200_proper_packet_is_unpacked(void)
+{
+    gen_test_input_packet_is_unpacked<nyse_protrac_execution_report_msg_t>
+        (
+            compose_random_msg,
+            format_message,
+            nyse_protrac_unpack_execution_report_msg,
+            assert_messages_are_equal,
+            NYSE_PROTRAC_EXECUTION_REPORT_MSG_SIZE,
+            m_random);
+}
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+void compose_random_msg(nyse_protrac_execution_report_cancellation_msg_t * const out_p)
+{
+    ANON_VAR(out_p->exec_time);
+    ANON_ARRAY(out_p->symbol);
+    ANON_VAR(out_p->volume);
+    ANON_VAR(out_p->link_id);
+    ANON_VAR(out_p->execution_type);
+}
+
+uint8_t * format_message(
+    nyse_protrac_execution_report_cancellation_msg_t const * const i_msg_p,
+    uint8_t * o_buffer,
+    const size_t o_buf_size)
+{
+    assert(o_buf_size >= NYSE_PROTRAC_EXECUTION_REPORT_CANCELLATION_MSG_SIZE);
+
+    size_t      random_offset = random() % (1 + o_buf_size - NYSE_PROTRAC_EXECUTION_REPORT_CANCELLATION_MSG_SIZE);
+
+    std::vector<uint8_t>    work_vec;
+
+    serialize(i_msg_p->exec_time, work_vec);
+    work_vec.insert(work_vec.end(), i_msg_p->symbol, i_msg_p->symbol + sizeof (i_msg_p->symbol));
+    serialize(i_msg_p->volume, work_vec);
+    serialize(i_msg_p->link_id, work_vec);
+    serialize(i_msg_p->execution_type, work_vec);
+
+    std::copy(work_vec.begin(), work_vec.end(), &o_buffer[random_offset]);
+
+    return &o_buffer[random_offset];
+}
+
+void assert_messages_are_equal(const nyse_protrac_execution_report_cancellation_msg_t & lhs, const nyse_protrac_execution_report_cancellation_msg_t & rhs)
+{
+    TS_ASSERT_EQUALS(lhs.exec_time,             rhs.exec_time);
+    TS_ASSERT_SAME_DATA(lhs.symbol, rhs.symbol, sizeof (lhs.symbol));
+    TS_ASSERT_EQUALS(lhs.volume,                rhs.volume);
+    TS_ASSERT_EQUALS(lhs.link_id,               rhs.link_id);
+    TS_ASSERT_EQUALS(lhs.execution_type,        rhs.execution_type);
+}
+
+class NyseProtracUnpackExecutionReportCancellationMsg : public CxxTest::TestSuite
+{
+private:
+
+testing::internal::Random * m_random;
+
+public:
+
+NyseProtracUnpackExecutionReportCancellationMsg()
+{
+    m_random = new testing::internal::Random(time(0));
+    srandom(time(0));
+}
+~NyseProtracUnpackExecutionReportCancellationMsg()
+{
+    delete m_random;
+}
+
+void test_00100_fails_when_input_packet_ptr_is_null(void)
+{
+    gen_test_input_packet_ptr_is_null<nyse_protrac_execution_report_cancellation_msg_t>
+        (nyse_protrac_unpack_execution_report_cancellation_msg, m_random);
+}
+
+void test_00101_fails_when_input_packet_is_too_short(void)
+{
+    gen_test_input_packet_is_too_short<nyse_protrac_execution_report_cancellation_msg_t>
+        (nyse_protrac_unpack_execution_report_cancellation_msg, NYSE_PROTRAC_EXECUTION_REPORT_CANCELLATION_MSG_SIZE, m_random);
+}
+
+void test_00200_proper_packet_is_unpacked(void)
+{
+    gen_test_input_packet_is_unpacked<nyse_protrac_execution_report_cancellation_msg_t>
+        (
+            compose_random_msg,
+            format_message,
+            nyse_protrac_unpack_execution_report_cancellation_msg,
+            assert_messages_are_equal,
+            NYSE_PROTRAC_EXECUTION_REPORT_CANCELLATION_MSG_SIZE,
+            m_random);
+}
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+void compose_random_msg(nyse_protrac_summary_msg_t * const out_p)
+{
+    ANON_ARRAY(out_p->symbol);
+    ANON_VAR(out_p->total_volume);
+    ANON_VAR(out_p->execution_type);
+}
+
+uint8_t * format_message(
+    nyse_protrac_summary_msg_t const * const i_msg_p,
+    uint8_t * o_buffer,
+    const size_t o_buf_size)
+{
+    assert(o_buf_size >= NYSE_PROTRAC_SUMMARY_MSG_SIZE);
+
+    size_t      random_offset = random() % (1 + o_buf_size - NYSE_PROTRAC_SUMMARY_MSG_SIZE);
+
+    std::vector<uint8_t>    work_vec;
+
+    work_vec.insert(work_vec.end(), i_msg_p->symbol, i_msg_p->symbol + sizeof (i_msg_p->symbol));
+    serialize(i_msg_p->total_volume, work_vec);
+    serialize(i_msg_p->execution_type, work_vec);
+
+    std::copy(work_vec.begin(), work_vec.end(), &o_buffer[random_offset]);
+
+    return &o_buffer[random_offset];
+}
+
+void assert_messages_are_equal(const nyse_protrac_summary_msg_t & lhs, const nyse_protrac_summary_msg_t & rhs)
+{
+    TS_ASSERT_SAME_DATA(lhs.symbol, rhs.symbol, sizeof (lhs.symbol));
+    TS_ASSERT_EQUALS(lhs.total_volume,          rhs.total_volume);
+    TS_ASSERT_EQUALS(lhs.execution_type,        rhs.execution_type);
+}
+
+class NyseProtracUnpackSummaryMsg : public CxxTest::TestSuite
+{
+private:
+
+testing::internal::Random * m_random;
+
+public:
+
+NyseProtracUnpackSummaryMsg()
+{
+    m_random = new testing::internal::Random(time(0));
+    srandom(time(0));
+}
+~NyseProtracUnpackSummaryMsg()
+{
+    delete m_random;
+}
+
+void test_00100_fails_when_input_packet_ptr_is_null(void)
+{
+    gen_test_input_packet_ptr_is_null<nyse_protrac_summary_msg_t>
+        (nyse_protrac_unpack_summary_msg, m_random);
+}
+
+void test_00101_fails_when_input_packet_is_too_short(void)
+{
+    gen_test_input_packet_is_too_short<nyse_protrac_summary_msg_t>
+        (nyse_protrac_unpack_summary_msg, NYSE_PROTRAC_SUMMARY_MSG_SIZE, m_random);
+}
+
+void test_00200_proper_packet_is_unpacked(void)
+{
+    gen_test_input_packet_is_unpacked<nyse_protrac_summary_msg_t>
+        (
+            compose_random_msg,
+            format_message,
+            nyse_protrac_unpack_summary_msg,
+            assert_messages_are_equal,
+            NYSE_PROTRAC_SUMMARY_MSG_SIZE,
+            m_random);
+}
+
+};
